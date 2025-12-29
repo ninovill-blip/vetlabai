141eec103b31b9a761f851367124a1e393953b26
ninovill-blip <ninovill@gmail.com>
2025-12-29 02:59:12 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -537,7 +537,7 @@ If your pet has mysterious symptoms or you just want to understand their health 
   src="/LOGO.png" 
   alt="PETLABAI - Holistic First" 
   style={{ 
-    height: '180px',
+    height: '220px',
     width: 'auto'
   }}
 />
