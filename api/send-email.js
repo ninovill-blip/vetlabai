43fe7486c3f314583482cda753c50e63846e246e
ninovill-blip <ninovill@gmail.com>
2025-12-31 15:57:23 -0800

    Add default API key for Resend initialization
    Set a default value for RESEND_API_KEY.

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || 're_HJv6YJ1D_KrmynRqwabxGipfr6Pdnaiue');
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
